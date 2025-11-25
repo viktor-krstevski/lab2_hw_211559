200cf90e5cf7c5661ab81999013b58c614224831
Viktor Krstevski <viktor5krstevski@gmail.com>
2025-11-25 18:47:08 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'categories_screen.dart';
+
+void main() {
+  runApp(const RecipesApp());
+}
+
+class RecipesApp extends StatelessWidget {
+  const RecipesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipes App',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+      ),
+      home: const CategoriesScreen(),
+    );
+  }
+}
